7949bc686ed34fa280395f6cbbb7808ee5082e5b
lassesilk <lassesilkoset@gmail.com>
2022-10-03 13:33:20 +0200

    Initial Commit

---
+++ CustomButtonTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomButtonTutorial
+//
+//  Created by Lasse Silkoset on 03/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomButtonTutorial/CustomButtonTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonTutorialApp.swift
+//  CustomButtonTutorial
+//
+//  Created by Lasse Silkoset on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
